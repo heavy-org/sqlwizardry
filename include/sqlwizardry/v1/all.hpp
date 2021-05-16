1761b4b3b5d608427990c4a4ae4ff0ce600ecbcf
Joshua Langley <Joshua.langley.1995@gmail.com>
2021-05-16 22:22:25 +0100

    Merge problem

--- include/sqlwizardry/v1/all.hpp
+++ include/sqlwizardry/v1/all.hpp
@@ -4,4 +4,8 @@
 #include <sqlwizardry/v1/serialiser.hpp>
 #include <sqlwizardry/v1/engine/debug.hpp>
 #include <sqlwizardry/v1/engine/mysql.hpp>
+<<<<<<< Updated upstream
 #include <sqlwizardry/v1/macro.hpp>
+=======
+#include <sqlwizardry/v1/macro.hpp>
+>>>>>>> Stashed changes
