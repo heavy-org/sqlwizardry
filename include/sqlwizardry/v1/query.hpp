bd0109903cd820a5a949350aa7ece95e837b47e6
Restyled.io <commits@restyled.io>
2021-05-18 00:33:56 +0000

    Restyled by clang-format

--- include/sqlwizardry/v1/query.hpp
+++ include/sqlwizardry/v1/query.hpp
@@ -22,33 +22,33 @@ namespace detail
 template <typename COLUMNS>
 constexpr auto get_ordering(COLUMNS column)
 {
-    if constexpr (is_ordering_v<std::decay_t<COLUMNS>>)
-    {
-        return column;
-    }
-    else
-    {
-        return column::Ascending{std::move(column)};
-    }
+  if constexpr (is_ordering_v<std::decay_t<COLUMNS>>)
+  {
+    return column;
+  }
+  else
+  {
+    return column::Ascending{std::move(column)};
+  }
 }
 
 template <typename... COLUMNS>
 constexpr auto get_ordering_tuple(COLUMNS... cols)
 {
-    return std::make_tuple(get_ordering(std::move(cols))...);
+  return std::make_tuple(get_ordering(std::move(cols))...);
 }
 
 template <typename MODEL, typename DB, typename... T>
 constexpr auto make_query(DB& db, T&&... ELEMENTS)
 {
-    return Query<MODEL, DB, std::decay_t<T>...> {db, std::forward<T>(ELEMENTS)...};
+  return Query<MODEL, DB, std::decay_t<T>...>{db, std::forward<T>(ELEMENTS)...};
 }
 }  // namespace detail
 
 template <typename... COLUMNS>
 constexpr auto get_ordering_tuple(COLUMNS... cols)
 {
-    return std::make_tuple(detail::get_ordering(std::move(cols))...);
+  return std::make_tuple(detail::get_ordering(std::move(cols))...);
 }
 
 template <typename MODEL, typename DATABASE, typename... ELEMENTS>
@@ -57,128 +57,116 @@ struct AllQuery;
 template <typename MODEL, typename DATABASE, typename... ELEMENTS>
 class Query
 {
-protected:
-    DATABASE& db;
-    type_of_t<select_tag, ELEMENTS...> _select;
-    type_of_t<where_tag, ELEMENTS...> _where;
-    type_of_t<order_tag, ELEMENTS...> _order_by;
-    type_of_t<limit_tag, ELEMENTS...> _limit;
-    type_of_t<offset_tag, ELEMENTS...> _offset;
+ protected:
+  DATABASE& db;
+  type_of_t<select_tag, ELEMENTS...> _select;
+  type_of_t<where_tag, ELEMENTS...> _where;
+  type_of_t<order_tag, ELEMENTS...> _order_by;
+  type_of_t<limit_tag, ELEMENTS...> _limit;
+  type_of_t<offset_tag, ELEMENTS...> _offset;
 
-public:
-    constexpr Query(DATABASE& db, ELEMENTS... e)
-        : db{db},
-          _select{std::move(value_of<select_tag>(e...))},
-          _where{std::move(value_of<where_tag>(e...))},
-          _order_by{std::move(value_of<order_tag>(e...))},
-          _limit{std::move(value_of<limit_tag>(e...))},
-          _offset{std::move(value_of<offset_tag>(e...))}
+ public:
+  constexpr Query(DATABASE& db, ELEMENTS... e)
+      : db{db},
+        _select{std::move(value_of<select_tag>(e...))},
+        _where{std::move(value_of<where_tag>(e...))},
+        _order_by{std::move(value_of<order_tag>(e...))},
+        _limit{std::move(value_of<limit_tag>(e...))},
+        _offset{std::move(value_of<offset_tag>(e...))}
+  {
+  }
+
+  template <typename... COLUMNS>
+  [[nodiscard]] constexpr auto select(COLUMNS&&... cols)
+  {
+    static_assert(is_empty_v<decltype(_select.value)>, "Selection already made in this query,");
+    if constexpr (sizeof...(cols) == 0)
     {
+      auto fields = get_columns<MODEL>();
+      return detail::make_query<MODEL>(db, kv<select_tag>(std::move(fields)), std::move(_where), std::move(_order_by), std::move(_limit),
+                                       std::move(_offset));
     }
-
-    template <typename... COLUMNS>
-    [[nodiscard]] constexpr auto select(COLUMNS&&... cols)
+    else
     {
-        static_assert(is_empty_v<decltype(_select.value)>, "Selection already made in this query,");
-        if constexpr (sizeof...(cols) == 0)
-        {
-            auto fields = get_columns<MODEL>();
-            return detail::make_query<MODEL>(db, kv<select_tag>(std::move(fields)), std::move(_where), std::move(_order_by), std::move(_limit),
-                                             std::move(_offset));
-        }
-        else
-        {
-            return detail::make_query<MODEL>(db, kv<select_tag>(std::make_tuple(cols...)), std::move(_where), std::move(_order_by),
-                                             std::move(_limit), std::move(_offset));
-        }
+      return detail::make_query<MODEL>(db, kv<select_tag>(std::make_tuple(cols...)), std::move(_where), std::move(_order_by),
+                                       std::move(_limit), std::move(_offset));
     }
+  }
 
-    template <typename PREDICATE>
-    [[nodiscard]] constexpr auto where(PREDICATE&& pred)
+  template <typename PREDICATE>
+  [[nodiscard]] constexpr auto where(PREDICATE&& pred)
+  {
+    if constexpr (is_empty_v<decltype(_where.value)>)
     {
-        if constexpr (is_empty_v<decltype(_where.value)>)
-        {
-            return detail::make_query<MODEL>(db, std::move(_select), kv<where_tag>(std::forward<PREDICATE>(pred)), std::move(_order_by),
-                                             std::move(_limit), std::move(_offset));
-        }
-        else
-        {
-            return detail::make_query<MODEL>(db, std::move(_select), kv<where_tag>(_where.value && std::forward<PREDICATE>(pred)),
-                                             std::move(_order_by), std::move(_limit), std::move(_offset));
-        }
+      return detail::make_query<MODEL>(db, std::move(_select), kv<where_tag>(std::forward<PREDICATE>(pred)), std::move(_order_by),
+                                       std::move(_limit), std::move(_offset));
     }
-
-    template <typename... ORDERING>
-    [[nodiscard]] constexpr auto order_by(ORDERING&&... order)
+    else
     {
-        static_assert(is_empty_v<decltype(_order_by.value)>, "This query already has an ordering specified");
-        auto ordering = get_ordering_tuple(std::forward<ORDERING>(order)...);
-        return detail::make_query<MODEL>(db, std::move(_select), std::move(_where), kv<order_tag>(std::move(ordering)), std::move(_limit),
-                                         std::move(_offset));
+      return detail::make_query<MODEL>(db, std::move(_select), kv<where_tag>(_where.value && std::forward<PREDICATE>(pred)),
+                                       std::move(_order_by), std::move(_limit), std::move(_offset));
     }
+  }
 
-    [[nodiscard]] constexpr auto limit(std::size_t l)
-    {
-        return detail::make_query<MODEL>(db, std::move(_select), std::move(_where), std::move(_order_by), kv<limit_tag>(l), std::move(_offset));
-    }
+  template <typename... ORDERING>
+  [[nodiscard]] constexpr auto order_by(ORDERING&&... order)
+  {
+    static_assert(is_empty_v<decltype(_order_by.value)>, "This query already has an ordering specified");
+    auto ordering = get_ordering_tuple(std::forward<ORDERING>(order)...);
+    return detail::make_query<MODEL>(db, std::move(_select), std::move(_where), kv<order_tag>(std::move(ordering)), std::move(_limit),
+                                     std::move(_offset));
+  }
 
-    template <std::size_t L>
-    [[nodiscard]] constexpr auto limit()
-    {
-        static_assert(L > 0, "Limit must be greater than 0");
-        return detail::make_query<MODEL>(db, std::move(_select), std::move(_where), std::move(_order_by), kv<limit_tag>(L), std::move(_offset));
-    }
+  [[nodiscard]] constexpr auto limit(std::size_t l)
+  {
+    return detail::make_query<MODEL>(db, std::move(_select), std::move(_where), std::move(_order_by), kv<limit_tag>(l), std::move(_offset));
+  }
 
-    [[nodiscard]] constexpr auto offset(std::size_t l)
-    {
-        return detail::make_query<MODEL>(db, std::move(_select), std::move(_where), std::move(_order_by), std::move(_limit), kv<offset_tag>(l));
-    }
+  template <std::size_t L>
+  [[nodiscard]] constexpr auto limit()
+  {
+    static_assert(L > 0, "Limit must be greater than 0");
+    return detail::make_query<MODEL>(db, std::move(_select), std::move(_where), std::move(_order_by), kv<limit_tag>(L), std::move(_offset));
+  }
 
-    template <std::size_t L>
-    [[nodiscard]] constexpr auto offset()
-    {
-        return detail::make_query<MODEL>(db, std::move(_select), std::move(_where), std::move(_order_by), std::move(_limit), kv<offset_tag>(L));
-    }
+  [[nodiscard]] constexpr auto offset(std::size_t l)
+  {
+    return detail::make_query<MODEL>(db, std::move(_select), std::move(_where), std::move(_order_by), std::move(_limit), kv<offset_tag>(l));
+  }
 
-    [[nodiscard]] constexpr const auto& get_select() const {
-        return _select.value;
-    }
+  template <std::size_t L>
+  [[nodiscard]] constexpr auto offset()
+  {
+    return detail::make_query<MODEL>(db, std::move(_select), std::move(_where), std::move(_order_by), std::move(_limit), kv<offset_tag>(L));
+  }
 
-    [[nodiscard]] constexpr const auto& get_where() const {
-        return _where.value;
-    }
+  [[nodiscard]] constexpr const auto& get_select() const { return _select.value; }
 
-    [[nodiscard]] constexpr const auto& get_order() const {
-        return _order_by.value;
-    }
+  [[nodiscard]] constexpr const auto& get_where() const { return _where.value; }
 
-    [[nodiscard]] constexpr const auto& get_limit() const {
-        return _limit.value;
-    }
+  [[nodiscard]] constexpr const auto& get_order() const { return _order_by.value; }
 
-    [[nodiscard]] constexpr const auto& get_offset() const {
-        return _offset.value;
-    }
+  [[nodiscard]] constexpr const auto& get_limit() const { return _limit.value; }
 
-    [[nodiscard]] constexpr auto& get_db() {
-        return db;
-    }
+  [[nodiscard]] constexpr const auto& get_offset() const { return _offset.value; }
 
-    [[nodiscard]] constexpr auto all()
-    {
-        static_assert(!is_empty_v<decltype(_select.value)>, "This query needs at leas one selection. Did you forget to call .select()?");
-        return AllQuery<MODEL, DATABASE, ELEMENTS...> {*this};
-    }
+  [[nodiscard]] constexpr auto& get_db() { return db; }
+
+  [[nodiscard]] constexpr auto all()
+  {
+    static_assert(!is_empty_v<decltype(_select.value)>, "This query needs at leas one selection. Did you forget to call .select()?");
+    return AllQuery<MODEL, DATABASE, ELEMENTS...>{*this};
+  }
 };
 
 template <typename CRTP>
 struct model
 {
-    template <typename DB>
-    [[nodiscard]] static constexpr auto query(DB& database)
-    {
-        return Query<CRTP, DB> {database};
-    }
+  template <typename DB>
+  [[nodiscard]] static constexpr auto query(DB& database)
+  {
+    return Query<CRTP, DB>{database};
+  }
 };
 
 template <typename MODEL, typename DB, typename... ELEMENTS>
@@ -187,20 +175,20 @@ struct query_serialiser;
 template <typename MODEL, typename DATABASE, typename... ELEMENTS>
 class AllQuery : private Query<MODEL, DATABASE, ELEMENTS...>
 {
-    using QUERY_T = Query<MODEL, DATABASE, ELEMENTS...>;
+  using QUERY_T = Query<MODEL, DATABASE, ELEMENTS...>;
 
-public:
-    constexpr AllQuery(Query<MODEL, DATABASE, ELEMENTS...> q) : QUERY_T{std::move(q)} {}
+ public:
+  constexpr AllQuery(Query<MODEL, DATABASE, ELEMENTS...> q) : QUERY_T{std::move(q)} {}
 
-    auto begin()
-    {
-        std::stringstream ss;
-        query_serialiser{*this}(ss);
-        auto& db = QUERY_T::get_db();
-        db.run(ss.str());
-    }
+  auto begin()
+  {
+    std::stringstream ss;
+    query_serialiser{*this}(ss);
+    auto& db = QUERY_T::get_db();
+    db.run(ss.str());
+  }
 
-    auto end() {}
+  auto end() {}
 };
 }  // namespace sqlwizardry
 
--- test/unit/declaration.cpp
+++ test/unit/declaration.cpp
@@ -7,70 +7,70 @@ SQLWIZARDRY_TABLE(User, "user", (Column<std::string>)username, (Column<std::stri
 
 TEST(Declaration, BasicTableDeclarationSelect)
 {
-    std::stringstream ss;
-    sqlwizardry::engine::Debug debugEngine{ss};
-    auto results = User::query(debugEngine).select().all();
-    results.begin();
-    EXPECT_EQ(ss.str(), "SELECT user.username, user.password FROM user\n");
+  std::stringstream ss;
+  sqlwizardry::engine::Debug debugEngine{ss};
+  auto results = User::query(debugEngine).select().all();
+  results.begin();
+  EXPECT_EQ(ss.str(), "SELECT user.username, user.password FROM user\n");
 }
 
 TEST(Declaration, BasicTableDeclarationSelectWithPred)
 {
-    std::stringstream ss;
-    sqlwizardry::engine::Debug debugEngine{ss};
-    auto results = User::query(debugEngine).select().where(User::username == "Heavyhat").all();
-    results.begin();
-    EXPECT_EQ(ss.str(), "SELECT user.username, user.password FROM user WHERE user.username = \"Heavyhat\"\n");
+  std::stringstream ss;
+  sqlwizardry::engine::Debug debugEngine{ss};
+  auto results = User::query(debugEngine).select().where(User::username == "Heavyhat").all();
+  results.begin();
+  EXPECT_EQ(ss.str(), "SELECT user.username, user.password FROM user WHERE user.username = \"Heavyhat\"\n");
 }
 
 TEST(Declaration, BasicTableDeclarationSelectWithPredAndOrder)
 {
-    std::stringstream ss;
-    sqlwizardry::engine::Debug debugEngine{ss};
-    auto results = User::query(debugEngine).select().where(User::username == "Heavyhat").order_by(User::username).all();
-    results.begin();
-    EXPECT_EQ(ss.str(), "SELECT user.username, user.password FROM user WHERE user.username = \"Heavyhat\" ORDER BY user.username ASC\n");
+  std::stringstream ss;
+  sqlwizardry::engine::Debug debugEngine{ss};
+  auto results = User::query(debugEngine).select().where(User::username == "Heavyhat").order_by(User::username).all();
+  results.begin();
+  EXPECT_EQ(ss.str(), "SELECT user.username, user.password FROM user WHERE user.username = \"Heavyhat\" ORDER BY user.username ASC\n");
 }
 
 TEST(Declaration, BasicTableDeclarationSelectWithPredAndOrderAndLimit)
 {
-    std::stringstream ss;
-    sqlwizardry::engine::Debug debugEngine{ss};
-    auto results = User::query(debugEngine).select().where(User::username == "Heavyhat").order_by(User::username).limit(1).all();
-    results.begin();
-    EXPECT_EQ(ss.str(),
-              "SELECT user.username, user.password FROM user WHERE user.username = \"Heavyhat\" ORDER BY user.username ASC LIMIT 1\n");
+  std::stringstream ss;
+  sqlwizardry::engine::Debug debugEngine{ss};
+  auto results = User::query(debugEngine).select().where(User::username == "Heavyhat").order_by(User::username).limit(1).all();
+  results.begin();
+  EXPECT_EQ(ss.str(),
+            "SELECT user.username, user.password FROM user WHERE user.username = \"Heavyhat\" ORDER BY user.username ASC LIMIT 1\n");
 }
 
 TEST(Declaration, BasicTableDeclarationSelectWithPredAndOrderAndLimit2)
 {
-    std::stringstream ss;
-    sqlwizardry::engine::Debug debugEngine{ss};
-    auto results = User::query(debugEngine).select().where(User::username == "Heavyhat").order_by(User::username).limit<1>().all();
-    results.begin();
-    EXPECT_EQ(ss.str(),
-              "SELECT user.username, user.password FROM user WHERE user.username = \"Heavyhat\" ORDER BY user.username ASC LIMIT 1\n");
+  std::stringstream ss;
+  sqlwizardry::engine::Debug debugEngine{ss};
+  auto results = User::query(debugEngine).select().where(User::username == "Heavyhat").order_by(User::username).limit<1>().all();
+  results.begin();
+  EXPECT_EQ(ss.str(),
+            "SELECT user.username, user.password FROM user WHERE user.username = \"Heavyhat\" ORDER BY user.username ASC LIMIT 1\n");
 }
 
 TEST(Declaration, BasicTableDeclarationSelectWithPredAndOrderAndLimitAndOffSet)
 {
-    std::stringstream ss;
-    sqlwizardry::engine::Debug debugEngine{ss};
-    auto results = User::query(debugEngine).select().where(User::username == "Heavyhat").order_by(User::username).limit(1).offset(1).all();
-    results.begin();
-    EXPECT_EQ(
-        ss.str(),
-        "SELECT user.username, user.password FROM user WHERE user.username = \"Heavyhat\" ORDER BY user.username ASC LIMIT 1 OFFSET 1\n");
+  std::stringstream ss;
+  sqlwizardry::engine::Debug debugEngine{ss};
+  auto results = User::query(debugEngine).select().where(User::username == "Heavyhat").order_by(User::username).limit(1).offset(1).all();
+  results.begin();
+  EXPECT_EQ(
+      ss.str(),
+      "SELECT user.username, user.password FROM user WHERE user.username = \"Heavyhat\" ORDER BY user.username ASC LIMIT 1 OFFSET 1\n");
 }
 
 TEST(Declaration, BasicTableDeclarationSelectWithPredAndOrderAndLimitAndOffset2)
 {
-    std::stringstream ss;
-    sqlwizardry::engine::Debug debugEngine{ss};
-    auto results =
-        User::query(debugEngine).select().where(User::username == "Heavyhat").order_by(User::username).limit<1>().offset<1>().all();
-    results.begin();
-    EXPECT_EQ(
-        ss.str(),
-        "SELECT user.username, user.password FROM user WHERE user.username = \"Heavyhat\" ORDER BY user.username ASC LIMIT 1 OFFSET 1\n");
+  std::stringstream ss;
+  sqlwizardry::engine::Debug debugEngine{ss};
+  auto results =
+      User::query(debugEngine).select().where(User::username == "Heavyhat").order_by(User::username).limit<1>().offset<1>().all();
+  results.begin();
+  EXPECT_EQ(
+      ss.str(),
+      "SELECT user.username, user.password FROM user WHERE user.username = \"Heavyhat\" ORDER BY user.username ASC LIMIT 1 OFFSET 1\n");
 }
